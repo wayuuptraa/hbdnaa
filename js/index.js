9079ab9dc3d0825054563d9d5e8f1bafede490f5
wayuuptraa <deliaptri15@gmail.com>
2025-04-22 13:44:29 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'I Have Something'.split('')
+const text = 'i have something for youu'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
